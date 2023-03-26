77f41c5aba3c5ce395fb47416343508d1dffd365
Nurjamal <n.mirbaizaeva@gmail.com>
2023-03-26 21:50:11 +0600

    Initial Commit

---
+++ hw7month3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw7month3
+//
+//  Created by Nurjamal Mirbaizaeva on 26/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
